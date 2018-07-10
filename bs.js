9557737671f4ae9c0e55a2d3778227c87349430b
Dan Rader <howdy@danrdesign.com>
2018-07-10 16:58:00 -0400

    Initial commit

---
+++ bs.js
@@ -0,0 +1,5 @@
+var browserSync = require('browser-sync');
+
+browserSync({
+  proxy: 'https://isl.co/'
+});
---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var browserSync = require('browser-sync');
+
+browserSync({
+  proxy: 'https://isl.co/'
+});
+
+// Task for serving blog with Browsersync
+gulp.task('serve', function () {
+    browserSync.init({server: {baseDir: '_site/'}});
+    // Reloads page when some of the already built files changed:
+    gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
